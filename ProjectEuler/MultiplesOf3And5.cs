0546cc16b1b84aaedc5c89ec35e4db3c08457f47
Kaniarasan Nagalingam <kaniarasan.nagalingam@pearson.com>
2019-01-08 14:19:01 +0530

    Added initial

---
+++ ProjectEuler/MultiplesOf3And5.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace ProjectEuler
+{
+    public class MultiplesOf3And5
+    {
+        public int GetMultiples()
+        {
+            var range = Enumerable.Range(0, 1000);
+            return range.Where(x => (x % 3) == 0 || (x % 5) == 0).Select(x => x).Sum();
+        }
+    }
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var obj =new MultiplesOf3And5();
+            Console.WriteLine(obj.GetMultiples());
+            Console.ReadLine();
+        }
+    }
+}
