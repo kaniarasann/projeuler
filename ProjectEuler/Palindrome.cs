c80b98656a18c21ca6c3b38885a3212a2980ada3
Kaniarasan Nagalingam <kaniarasan.nagalingam@pearson.com>
2019-01-19 10:02:54 +0530

    Palindrome_Check_Completed

---
+++ ProjectEuler/Palindrome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+   public class Palindrome
+    {
+        public long GetPalindrome() {
+            var i = Enumerable.Range(1, 999);
+            var j = i.SelectMany(x => i, (x, y) => {
+                return (x * y).ToString();
+               
+            }).Where (x=> x == string.Join(string.Empty,x.Reverse())).Select(x=>Convert.ToInt32(x)).Max();
+            return 1;
+        }
+      
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -13,9 +13,12 @@ namespace ProjectEuler
             Stopwatch stopwatch = Stopwatch.StartNew();
             //var obj = new FibanocciUpto4m();
             //Console.WriteLine(obj.GetOutput(4000000));
-            var obj = new PrimeFactor(600851475143);
-            Console.WriteLine(obj.GetMaxPrimeFactorNo());
+            //var obj = new PrimeFactor(600851475143);
+            //Console.WriteLine(obj.GetMaxPrimeFactorNo());
+            //var obj = new Palindrome();
+            //Console.WriteLine(obj.GetPalindrome());
             Console.WriteLine(stopwatch.Elapsed);
+            stopwatch.Stop();
             Console.ReadLine();
         }
     }
